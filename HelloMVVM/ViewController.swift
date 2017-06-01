cb24a76f8a3a0d60f88270c2f33ce47cda6c5158
ranjith@f22labs.com <ranjith@f22labs.com>
2017-06-01 08:14:16 +0530

    Initial Commit

---
+++ HelloMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloMVVM
+//
+//  Created by Ranjith Kumar on 01/06/2017.
+//  Copyright © 2017 F22Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
